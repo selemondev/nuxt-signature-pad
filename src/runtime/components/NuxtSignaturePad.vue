81d141f5965984e5c8e1279ef2c8e583ef17335c
selemondev <selemondev19@gmail.com>
2025-10-26 20:18:58 -0600

    feat: emit canvas data

--- src/runtime/components/NuxtSignaturePad.vue
+++ src/runtime/components/NuxtSignaturePad.vue
@@ -3,38 +3,44 @@
 <!-- eslint-disable @typescript-eslint/unified-signatures -->
 <!-- eslint-disable ts/ban-ts-comment -->
 <!-- eslint-disable ts/no-unused-expressions -->
-<script setup lang='ts'>
-import SignaturePad from 'signature_pad'
-import { nanoid } from 'nanoid'
-import { onMounted, onUnmounted, ref, watch, watchEffect } from 'vue'
-import type { CanvasOptions, Point, Props, Signature, WaterMarkObj } from '../types'
+<script setup lang="ts">
+import SignaturePad from "signature_pad";
+import { nanoid } from "nanoid";
+import { onMounted, onUnmounted, ref, watch, watchEffect } from "vue";
+import type {
+  CanvasOptions,
+  Point,
+  Props,
+  Signature,
+  WaterMarkObj,
+} from "../types";
 
 defineOptions({
-  name: 'VueSignaturePad',
+  name: "VueSignaturePad",
   inheritAttrs: false,
-})
+});
 
 const props = withDefaults(defineProps<Props>(), {
   throttle: 16,
   minWidth: 5,
   maxWidth: 5,
-  height: '100%',
-  width: '100%',
+  height: "100%",
+  width: "100%",
   options: () => ({
-    backgroundColor: 'rgb(255,255,255)',
-    penColor: 'rgb(0, 0, 0)',
+    backgroundColor: "rgb(255,255,255)",
+    penColor: "rgb(0, 0, 0)",
   }),
   disabled: false,
   clearOnResize: false,
-  defaultUrl: '',
-})
+  defaultUrl: "",
+});
 
 const emits = defineEmits<{
-  (e: 'beginStroke', val: string): void
-  (e: 'endStroke', val: string): void
-  (e: 'beforeUpdateStroke', val: string): void
-  (e: 'afterUpdateStroke', val: string): void
-}>()
+  (e: "beginStroke", val: Signature): void;
+  (e: "endStroke", val: Signature): void;
+  (e: "beforeUpdateStroke", val: Signature): void;
+  (e: "afterUpdateStroke", val: Signature): void;
+}>();
 
 const canvasOptions = ref<CanvasOptions>({
   signaturePad: {} as Signature,
@@ -45,168 +51,200 @@ const canvasOptions = ref<CanvasOptions>({
   backgroundColor: props.options.backgroundColor,
   penColor: props.options.penColor,
   canvasUuid: `canvas${nanoid()}`,
-})
+});
 
 function isCanvasEmpty(): boolean {
-  return canvasOptions.value.signaturePad.isEmpty()
+  return canvasOptions.value.signaturePad.isEmpty();
 }
 
 function saveSignature(format?: string) {
-  return format ? canvasOptions.value.signaturePad.toDataURL(format) : canvasOptions.value.signaturePad?.toDataURL()
+  return format
+    ? canvasOptions.value.signaturePad.toDataURL(format)
+    : canvasOptions.value.signaturePad?.toDataURL();
 }
 
 function clearCanvas() {
-  return canvasOptions.value.signaturePad.clear()
+  return canvasOptions.value.signaturePad.clear();
 }
 
 function fromDataURL(url: string) {
-  return canvasOptions.value.signaturePad.fromDataURL(url)
+  return canvasOptions.value.signaturePad.fromDataURL(url);
 }
 
 function undo() {
-  const canvasData: Point[][] = canvasOptions.value.signaturePad.toData()
+  const canvasData: Point[][] = canvasOptions.value.signaturePad.toData();
   if (canvasData.length) {
-    canvasData.pop()
-    canvasOptions.value.signaturePad.fromData(canvasData)
+    canvasData.pop();
+    canvasOptions.value.signaturePad.fromData(canvasData);
   }
-};
+}
 
 function addWaterMark(data: WaterMarkObj) {
-  if (!(Object.prototype.toString.call(data) === '[object Object]')) {
-    throw new Error(`Expected Object, got ${typeof data}.`)
-  }
-  else {
+  if (!(Object.prototype.toString.call(data) === "[object Object]")) {
+    throw new Error(`Expected Object, got ${typeof data}.`);
+  } else {
     const textData = {
-      text: data.text || '',
+      text: data.text || "",
       x: data.x || 20,
       y: data.y || 20,
       sx: data.sx || 40,
       sy: data.sy || 40,
-    }
-    const VCanvas = document.getElementById(canvasOptions.value.canvasUuid) as HTMLCanvasElement
-    const ctx = VCanvas.getContext('2d')
+    };
+    const VCanvas = document.getElementById(
+      canvasOptions.value.canvasUuid,
+    ) as HTMLCanvasElement;
+    const ctx = VCanvas.getContext("2d");
     if (ctx) {
-      ctx.font = data.font || '20px sans-serif'
-      ctx.fillStyle = data.fillStyle || '#333'
-      ctx.strokeStyle = data.strokeStyle || '#333'
-      if (data.style === 'all') {
-        ctx.fillText(textData.text, textData.x, textData.y)
+      ctx.font = data.font || "20px sans-serif";
+      ctx.fillStyle = data.fillStyle || "#333";
+      ctx.strokeStyle = data.strokeStyle || "#333";
+      if (data.style === "all") {
+        ctx.fillText(textData.text, textData.x, textData.y);
 
-        ctx.strokeText(textData.text, textData.sx, textData.sy)
-      }
-      else if (data.style === 'stroke') {
-        ctx.strokeText(textData.text, textData.sx, textData.sy)
-      }
-      else {
-        ctx.fillText(textData.text, textData.x, textData.y)
+        ctx.strokeText(textData.text, textData.sx, textData.sy);
+      } else if (data.style === "stroke") {
+        ctx.strokeText(textData.text, textData.sx, textData.sy);
+      } else {
+        ctx.fillText(textData.text, textData.x, textData.y);
       }
     }
-    canvasOptions.value.signaturePad._isEmpty = false
+    canvasOptions.value.signaturePad._isEmpty = false;
   }
 }
 
 function resizeCanvas(c: HTMLCanvasElement) {
-  let url
+  let url;
   if (!isCanvasEmpty()) {
-    url = saveSignature()
+    url = saveSignature();
   }
-  const ratio = Math.max(window.devicePixelRatio || 1, 1)
-  const reg = /px/
-  c.width = props.width && reg.test(props?.width) ? Number.parseInt(props?.width.replace(/px/g, '')) * ratio : c.offsetWidth * ratio
-  c.height = props.height && reg.test(props?.height) ? Number.parseInt(props?.height.replace(/px/g, '')) * ratio : c.offsetHeight * ratio
-  c.getContext('2d')?.scale(ratio, ratio)
-  clearCanvas()
-  !props.clearOnResize && url !== undefined && fromDataURL(url)
-  props.waterMark && Object.keys(props.waterMark).length && addWaterMark(props.waterMark)
+  const ratio = Math.max(window.devicePixelRatio || 1, 1);
+  const reg = /px/;
+  c.width =
+    props.width && reg.test(props?.width)
+      ? Number.parseInt(props?.width.replace(/px/g, "")) * ratio
+      : c.offsetWidth * ratio;
+  c.height =
+    props.height && reg.test(props?.height)
+      ? Number.parseInt(props?.height.replace(/px/g, "")) * ratio
+      : c.offsetHeight * ratio;
+  c.getContext("2d")?.scale(ratio, ratio);
+  clearCanvas();
+  !props.clearOnResize && url !== undefined && fromDataURL(url);
+  props.waterMark &&
+    Object.keys(props.waterMark).length &&
+    addWaterMark(props.waterMark);
 }
 
 function handleBeginStroke() {
-  return canvasOptions.value.signaturePad.addEventListener('beginStroke', () => {
-    emits('beginStroke', 'Signature started')
-  })
+  return canvasOptions.value.signaturePad.addEventListener(
+    "beginStroke",
+    () => {
+      emits("beginStroke", canvasOptions.value.signaturePad);
+    },
+  );
 }
 
 function handleEndStroke() {
-  return canvasOptions.value.signaturePad.addEventListener('endStroke', () => {
-    emits('endStroke', 'Signature ended')
-  })
+  return canvasOptions.value.signaturePad.addEventListener("endStroke", () => {
+    emits("endStroke", canvasOptions.value.signaturePad);
+  });
 }
 
 function handleBeforeUpdateStroke() {
-  return canvasOptions.value.signaturePad.addEventListener('beforeUpdateStroke', () => {
-    emits('beforeUpdateStroke', 'Signature before update')
-  })
+  return canvasOptions.value.signaturePad.addEventListener(
+    "beforeUpdateStroke",
+    () => {
+      emits("beforeUpdateStroke", canvasOptions.value.signaturePad);
+    },
+  );
 }
 
 function handleAfterUpdateStroke() {
-  return canvasOptions.value.signaturePad.addEventListener('afterUpdateStroke', () => {
-    emits('afterUpdateStroke', 'Signature after update')
-  })
+  return canvasOptions.value.signaturePad.addEventListener(
+    "afterUpdateStroke",
+    () => {
+      emits("afterUpdateStroke", canvasOptions.value.signaturePad);
+    },
+  );
 }
-const controller = new AbortController()
+const controller = new AbortController();
 function draw() {
-  const canvas = document.getElementById(canvasOptions.value.canvasUuid) as HTMLCanvasElement
+  const canvas = document.getElementById(
+    canvasOptions.value.canvasUuid,
+  ) as HTMLCanvasElement;
   if (!canvas) {
-    throw new Error(`Canvas element with ID ${canvasOptions.value.canvasUuid} not found.`)
-  };
-  // @ts-ignore
-  canvasOptions.value.signaturePad = new SignaturePad(canvas, canvasOptions.value)
-  handleBeginStroke()
-  handleEndStroke()
-  handleBeforeUpdateStroke()
-  handleAfterUpdateStroke()
-  const resizeHandler = () => {
-    resizeCanvas(canvas)
+    throw new Error(
+      `Canvas element with ID ${canvasOptions.value.canvasUuid} not found.`,
+    );
   }
-  window.addEventListener('resize', resizeHandler, {
+  // @ts-ignore
+  canvasOptions.value.signaturePad = new SignaturePad(
+    canvas,
+    canvasOptions.value,
+  );
+  handleBeginStroke();
+  handleEndStroke();
+  handleBeforeUpdateStroke();
+  handleAfterUpdateStroke();
+  const resizeHandler = () => {
+    resizeCanvas(canvas);
+  };
+  window.addEventListener("resize", resizeHandler, {
     signal: controller.signal,
-  })
-  resizeCanvas(canvas)
-  if (props.defaultUrl !== '') {
-    fromDataURL(props.defaultUrl)
+  });
+  resizeCanvas(canvas);
+  if (props.defaultUrl !== "") {
+    fromDataURL(props.defaultUrl);
   }
   if (props.disabled) {
-    canvasOptions.value.signaturePad.off()
-  }
-  else {
-    canvasOptions.value.signaturePad.on()
+    canvasOptions.value.signaturePad.off();
+  } else {
+    canvasOptions.value.signaturePad.on();
   }
 }
 
 watchEffect(() => {
   // Update penColor
-  canvasOptions.value.penColor = props.options.penColor
-  canvasOptions.value.signaturePad.penColor = props.options.penColor
+  canvasOptions.value.penColor = props.options.penColor;
+  canvasOptions.value.signaturePad.penColor = props.options.penColor;
   // Update backgroundColor
-  canvasOptions.value.backgroundColor = props.options.backgroundColor
-  canvasOptions.value.signaturePad.backgroundColor = props.options.backgroundColor
-})
+  canvasOptions.value.backgroundColor = props.options.backgroundColor;
+  canvasOptions.value.signaturePad.backgroundColor =
+    props.options.backgroundColor;
+});
 
-watch(() => props.minWidth, (newVal) => {
-  if (newVal) {
-    canvasOptions.value.minWidth = newVal
-    canvasOptions.value.signaturePad.minWidth = newVal
-  }
-}, { immediate: true })
+watch(
+  () => props.minWidth,
+  (newVal) => {
+    if (newVal) {
+      canvasOptions.value.minWidth = newVal;
+      canvasOptions.value.signaturePad.minWidth = newVal;
+    }
+  },
+  { immediate: true },
+);
 
-watch(() => props.maxWidth, (newVal) => {
-  if (newVal) {
-    canvasOptions.value.maxWidth = newVal
-    canvasOptions.value.signaturePad.maxWidth = newVal
-  }
-}, { immediate: true })
+watch(
+  () => props.maxWidth,
+  (newVal) => {
+    if (newVal) {
+      canvasOptions.value.maxWidth = newVal;
+      canvasOptions.value.signaturePad.maxWidth = newVal;
+    }
+  },
+  { immediate: true },
+);
 
 watch(
   () => props.disabled,
   (val) => {
     if (val) {
-      return canvasOptions.value.signaturePad.off()
-    }
-    else {
-      return canvasOptions.value.signaturePad.on()
+      return canvasOptions.value.signaturePad.off();
+    } else {
+      return canvasOptions.value.signaturePad.on();
     }
   },
-)
+);
 defineExpose({
   saveSignature,
   clearCanvas,
@@ -214,27 +252,24 @@ defineExpose({
   isCanvasEmpty,
   fromDataURL,
   undo,
-})
+});
 
 onMounted(() => {
   setTimeout(() => {
-    draw()
-  }, 100)
-})
+    draw();
+  }, 100);
+});
 
 onUnmounted(() => {
-  controller.abort()
-})
+  controller.abort();
+});
 </script>
 
 <template>
-  <div
-    :style="{ width: props.width, height: props.height }"
-    @touchmove.prevent
-  >
+  <div :style="{ width: props.width, height: props.height }" @touchmove.prevent>
     <canvas
       :id="canvasOptions.canvasUuid"
-      style="width: 100%; height: 100%;"
+      style="width: 100%; height: 100%"
       :data-uid="canvasOptions.canvasUuid"
     />
   </div>
